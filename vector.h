03057d032deac42a3ec4e238e9bbe4282d0b0118
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-02-13 20:50:49 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,1 +1,12 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#include "stdafx.h"
+#include <cmath>
+#include <string.h>
 
+void printVector(double vx3, double vy3);
+double add(double a, double b);
+double substract(double a, double b);
+double multiplyNumVec(double n, double coord);
+
+#endif VECTOR_H
