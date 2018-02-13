307e0ab42277d455fa3c38b7b7cebd880fe58372
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-02-13 20:50:27 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,1 +1,24 @@
+#include "vector.h"
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
 
+void printVector(double vx3, double vy3)
+{
+	cout << "New vector is (" << vx3 << ';' << vy3 << ')' << endl;
+}
+
+double add(double a, double b)
+{
+	return a + b;
+}
+
+double substract(double a, double b)
+{
+	return a - b;
+}
+
+double multiplyNumVec(double n, double coord)
+{
+	return n * coord;
+}
