f397eea6475251c0cbf65d6d912b252d66e92623
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-02-24 00:00:29 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,19 @@
+#include "stdafx.h"
 #include "vector.h"
 #include "iostream"
 using namespace std;
+
+void printMenu()
+{
+	cout << "-------------------------------" << endl;
+	cout << "Choose the operation:" << endl;
+	cout << "S - Substract vectors" << endl;
+	cout << "A - Add vectors " << endl;
+	cout << "V - Multiply vector and vector" << endl;
+	cout << "N - Multiply number and vector" << endl;
+	cout << "L - Lengths of vector" << endl;
+	cout << "-------------------------------" << endl;
+}
 
 void printVector(double vx3, double vy3)
 {
@@ -20,4 +33,10 @@ double substract(double a, double b)
 double multiplyNumVec(double n, double coord)
 {
 	return n * coord;
+}
+
+double vectorLength(vector v)
+{
+	
+	return (sqrt(pow(v.x, 2) + pow(v.y, 2)));
 }
