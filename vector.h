83acaf156ff9205dbed1dc68638a29b0e80c6017
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-02-24 00:03:13 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -1,11 +1,19 @@
+typedef struct vector {
+	int x;
+	int y;
+}vector;
+
 #ifndef VECTOR_H
 #define VECTOR_H
 #include <cmath>
 #include <string.h>
+#include <iostream>
 
+void printMenu();
 void printVector(double vx3, double vy3);
 double add(double a, double b);
 double substract(double a, double b);
 double multiplyNumVec(double n, double coord);
+double vectorLength(vector v);
 
 #endif VECTOR_H
