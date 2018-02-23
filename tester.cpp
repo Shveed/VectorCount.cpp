35da51de41adf039e235ba29b6c4508a3107d5f7
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-02-23 23:57:55 +0300

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,69 +1,63 @@
-#include <iostream>
+#include "stdafx.h"
 #include "vector.h"
 using namespace std;
 
 int main()
 {
-	double vx1, vy1, vx2, vy2, vx3, vy3, num, mult, l;
+	double vx3, vy3, num, mult;
+	vector v1;
+	vector v2;
 	char op;
-	cout << "-------------------------------" << endl;
-	cout << "Choose the operation:"			  << endl;
-	cout << "S - Substract vectors"			  << endl;
-	cout << "A - Add vectors "				  << endl;
-	cout << "V - Multiply vector and vector"  << endl;
-	cout << "N - Multiply number and vector"  << endl;
-	cout << "L - Lengths of vector"			  << endl;
-	cout << "-------------------------------" << endl;
+	printMenu();
 	cin >> op;
-	
+
 	switch (op)
 	{
-		case 'a':
-		case 'A':
-			cout << "Enter x1 y1 x2 y2 coordinates" << endl;
-			cin >> vx1 >> vy1 >> vx2 >> vy2;
-			vx3 = add(vx1, vx2);
-			vy3 = add(vy1, vy2);
-			printVector(vx3, vy3);
-			break;
+	case 'a':
+	case 'A':
+		cout << "Enter x1 y1 x2 y2 coordinates" << endl;
+		cin >> v1.x >> v1.y >> v2.x >> v2.y;
+		vx3 = add(v1.x, v2.x);
+		vy3 = add(v1.y, v2.y);
+		printVector(vx3, vy3);
+		break;
 
-		case 's':
-		case 'S':
-			cout << "Enter x1 y1 x2 y2 coordinates" << endl;
-			cin >> vx1 >> vy1 >> vx2 >> vy2;
-			vx3 = substract(vx1, vx2);
-			vy3 = substract(vy1, vy2);
-			printVector(vx3, vy3);
-			break;
+	case 's':
+	case 'S':
+		cout << "Enter x1 y1 x2 y2 coordinates" << endl;
+		cin >> v1.x >> v1.y >> v2.x >> v2.y;
+		vx3 = substract(v1.x, v2.x);
+		vy3 = substract(v1.y, v2.y);
+		printVector(vx3, vy3);
+		break;
 
-		case 'v':
-		case 'V':
-			cout << "Enter x1 y1 x2 y2 coordinates" << endl;
-			cin >> vx1 >> vy1 >> vx2 >> vy2;
-			mult = vx1 * vx2 + vy1 * vy2;
-			cout << "Multiplication equals " << mult << endl;
-			break;
+	case 'v':
+	case 'V':
+		cout << "Enter x1 y1 x2 y2 coordinates" << endl;
+		cin >> v1.x >> v1.y >> v2.x >> v2.y;
+		mult = v1.x * v2.x + v1.y * v2.y;
+		cout << "Multiplication equals " << mult << endl;
+		break;
 
-		case 'n':
-		case 'N':
-			cout << "Enter x1 y1 coordinates" << endl;
-			cin >> vx1 >> vy1;
-			cout << "Enter number" << endl;
-			cin >> num;
-			vx3 = multiplyNumVec(num, vx1);
-			vy3 = multiplyNumVec(num, vy1);
-			printVector(vx3, vy3);
-			break;
+	case 'n':
+	case 'N':
+		cout << "Enter x1 y1 coordinates" << endl;
+		cin >> v1.x >> v1.y;
+		cout << "Enter number" << endl;
+		cin >> num;
+		vx3 = multiplyNumVec(num, v1.x);
+		vy3 = multiplyNumVec(num, v1.y);
+		printVector(vx3, vy3);
+		break;
 
-		case 'l':
-		case 'L':
-			cout << "Enter x1 y1 coordinates" << endl;
-			cin >> vx1 >> vy1;
-			l = sqrt(pow(vx1, 2) + pow(vy1, 2));
-			cout << "Length of your vector is " << l << endl;
-			break;
-		default:
-			cout << "Error" << endl;
+	case 'l':
+	case 'L':
+		cout << "Enter x1 y1 coordinates" << endl;
+		cin >> v1.x >> v1.y;
+		cout << "Length of your vector is " << vectorLength(v1) << endl;
+		break;
+	default:
+		cout << "Error" << endl;
 	}
 	system("pause");
 	return 0;
